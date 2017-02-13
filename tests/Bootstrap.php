a186a360725cdd0197cd830ae6dda5a59f4ee5f2
Eugene Myazin <meniam@gmail.com>
2017-02-13 16:31:05 +0300

    Small algo fix. New phpunit

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,7 +1,14 @@
 <?php
 
-define('LIBRARY_PATH',  dirname(__FILE__) . '/../src');
+use Composer\Autoload\ClassLoader;
+error_reporting(error_reporting() & ~E_USER_DEPRECATED);
 
+/**
+ * @var ClassLoader $loader
+ */
+$loader = require __DIR__.'/../vendor/autoload.php';
+
+define('LIBRARY_PATH',  dirname(__FILE__) . '/../src');
 set_include_path(realpath(LIBRARY_PATH) . PATH_SEPARATOR . get_include_path());
 
 require LIBRARY_PATH . '/Permutation/Exception.php';
